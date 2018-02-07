0e7730cf3abb524d00ee8de911db7a5c19913b71
yazan.khayal <yazan.khayal@gmail.com>
2018-02-07 14:04:56 +0200

    scound

--- project7.php
+++ project7.php
@@ -4,7 +4,8 @@
 <!--[if IE 9]>
 <html class="ie ie9"> <![endif]-->
 <!--[if gt IE 9]><!-->
-<html> <!--<![endif]-->
+<html>
+<!--<![endif]-->
 
 <head>
     <!-- Page Title -->
@@ -48,131 +49,207 @@
     <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
     <![endif]-->
 </head>
-<body>
-<div id="page-wrapper">
 
-    <?php include('header.php'); ?>
-    <div class="page-title-container">
-        <div class="container">
-            <div class="page-title pull-left">
-                <h2 class="entry-title">Kalkulácia</h2>
+<body>
+    <div id="page-wrapper">
+
+        <?php include('header.php'); ?>
+        <div class="page-title-container">
+            <div class="container">
+                <div class="page-title pull-left">
+                    <h2 class="entry-title">Kalkulácia</h2>
+                </div>
+                <ul class="breadcrumbs pull-right">
+                    <li><a href="index.php">O nás</a></li>
+                    <li class="active">Kalkulácia</li>
+                </ul>
             </div>
-            <ul class="breadcrumbs pull-right">
-                <li><a href="index.php">O nás</a></li>
-                <li class="active">Kalkulácia</li>
-            </ul>
         </div>
+
+        <section id="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 pull-right">
+                        <h1>Kalkulácia</h1>
+                        <p>Nam volutpat aliquam sem, in tristique felis luctus consectetur. In molestie quis velit eget mattis. Vivamus libero eros, euismod at bibendum vel, aliquet a nunc. Proin viverra eleifend metus, sit amet bibendum orci. Sed eget semper odio. Mauris fermentum porttitor augue eu vehicula. Quisque congue velit turpis, sit amet interdum quam facilisis in. Proin venenatis purus a ipsum porttitor, sed cursus libero bibendum. Aenean auctor felis et sagittis egestasonec lobortis.</p>
+                    </div>
+                    <div class="col-lg-9 listing-style3 hotel">
+                        <div class="row rowblock">
+                            <div class="col-md-12">
+
+                                <nav class="breadcrumb"><a href="/">Domov</a> » Kalkulácie</nav>
+
+                                <div class="title">
+                                    <h1>Kalkulácie</h1>
+                                    <div class="alertm">Pridané používateľom kull dňa Ne, 09/15/2013 - 11:32</div>
+                                    <p>Cenové ponuky vyhotovujeme bezplatne</p>
+                                    <p>Na Vaše dotazy Vám v čo najkratšom čase odpovieme</p>
+                                    <p>Informácie označené hviezdičkou je nutné vyplniť!!!</p>
+                                </div>
+
+                                <form class="form">
+
+                                    <div class="col-sm-6">
+                                        <label>Počet kusov <span> *</span></label>
+                                        <div class="form-group">
+                                            <input type="number" class="form-control" required="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <label>Počet krídel  <span> *</span></label>
+                                        <div class="form-group">
+                                            <input type="number" class="form-control" required="">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Rozmery</label>
+                                        <div class="form-group ">
+                                            <input type="number" class="form-control">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Typ rámu <span> *</span></label>
+                                        <div class="form-group">
+                                            <select class="form-control" required="">
+                                            <option value="" selected="selected">- Zvoľte -</option>
+                                        <option value="0">6 - komorové</option>
+                                        <option value="10">5 - komorové</option>
+                                        </select>
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="col-sm-6">
+                                        <label>Forma otvárania  <span> *</span></label>
+                                        <div class="form-group">
+                                            <select class="form-control" required="">
+                                            <option value="" selected="selected">- Zvoľte -</option>
+                                        <option value="0">Sklopné</option>
+                                        <option value="10">Otváravo-sklopné</option>
+                                        </select>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Povrchová úprava  <span> *</span></label>
+                                        <div class="form-group">
+                                            <select class="form-control" required="">
+                                            <option value="" selected="selected">- Zvoľte -</option>
+                                        <option value="0">Obojstranná farba</option>
+                                        <option value="10">Jednostranna farba</option>
+                                        <option value="0">Biela</option>
+                                        </select>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Meno a priezvisko  <span> *</span></label>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" required="">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Ńázov firmy </label>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="col-sm-6">
+                                        <label>Adresa  <span> *</span></label>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" required="">
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="col-sm-6">
+                                        <label>Telefónne číslo *  <span> *</span></label>
+                                        <div class="form-group">
+                                            <input type="number" class="form-control" required="">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Fax </label>
+                                        <div class="form-group">
+                                            <input type="number" class="form-control">
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="col-sm-6">
+                                        <label>E-mail</label>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-sm-6">
+                                        <label>Poznámka </label>
+                                        <div class="form-group">
+                                            <textarea type="number" class="form-control" rows="8"></textarea>
+                                            <p>Tu môžete vložiť všetky ostatné informácie a dotazy</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <input type="submit" value="Potvrdiť údaje" class="btn btn-primary" />
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include('footer.php') ?>
     </div>
 
-    <section id="content">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 pull-right">
-                    <h1>Kalkulácia</h1>
-                    <p>Nam volutpat aliquam sem, in tristique felis luctus consectetur. In molestie quis velit eget mattis. Vivamus libero eros, euismod at bibendum vel, aliquet a nunc. Proin viverra eleifend metus, sit amet bibendum orci. Sed eget semper odio. Mauris fermentum porttitor augue eu vehicula. Quisque congue velit turpis, sit amet interdum quam facilisis in. Proin venenatis purus a ipsum porttitor, sed cursus libero bibendum. Aenean auctor felis et sagittis egestasonec lobortis.</p>
-                </div>
-                <div class="col-lg-9 listing-style3 hotel">
-                    <article class="box">
-                        <figure class="col-sm-5 col-md-4">
-                            <a title="" href="detail.php" class="hover-effect"><img width="270" height="160" alt="" src="images/dor/6.png"></a>
-                        </figure>
-                        <div class="details col-xs-12 col-sm-7 col-md-8">
-                            <div>
-                                <div>
-                                    <h4 class="box-title">Test Title</h4>
-                                </div>
-                            </div>
-                            <div>
-                                <p>Nunc cursus libero purus ac congue ar lorem cursus ut sed vitae pulvinar massa idend porta nequetiam elerisque mi id, consectetur adipi deese cing elit maus fringilla bibe endum.</p>
-                                <div class="btn-div">
-                                    <a class="button btn-small full-width text-center" title="" href="detail.php">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="box">
-                        <figure class="col-sm-5 col-md-4">
-                            <a title="" href="detail.php" class="hover-effect"><img width="270" height="160" alt="" src="images/dor/7.png"></a>
-                        </figure>
-                        <div class="details col-xs-12 col-sm-7 col-md-8">
-                            <div>
-                                <div>
-                                    <h4 class="box-title">Test Title</h4>
-                                </div>
-                            </div>
-                            <div>
-                                <p>Nunc cursus libero purus ac congue ar lorem cursus ut sed vitae pulvinar massa idend porta nequetiam elerisque mi id, consectetur adipi deese cing elit maus fringilla bibe endum.</p>
-                                <div class="btn-div">
-                                    <a class="button btn-small full-width text-center" title="" href="detail.php">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                    <article class="box">
-                        <figure class="col-sm-5 col-md-4">
-                            <a title="" href="detail.php" class="hover-effect"><img width="270" height="160" alt="" src="images/dor/8.png"></a>
-                        </figure>
-                        <div class="details col-xs-12 col-sm-7 col-md-8">
-                            <div>
-                                <div>
-                                    <h4 class="box-title">Test Title</h4>
-                                </div>
-                            </div>
-                            <div>
-                                <p>Nunc cursus libero purus ac congue ar lorem cursus ut sed vitae pulvinar massa idend porta nequetiam elerisque mi id, consectetur adipi deese cing elit maus fringilla bibe endum.</p>
-                                <div class="btn-div">
-                                    <a class="button btn-small full-width text-center" title="" href="detail.php">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <ul class="pagination clearfix">
-                        <li class="prev disabled"><a href="#">Previous</a></li>
-                        <li class="active"><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li class="disabled"><span>...</span></li>
-                        <li><a href="#">5</a></li>
-                        <li class="next"><a href="#">Next</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
 
-    <?php include('footer.php') ?>
-</div>
+    <!-- Javascript -->
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery.noconflict.js"></script>
+    <script type="text/javascript" src="js/modernizr.2.7.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.1.10.4.min.js"></script>
 
+    <!-- Twitter Bootstrap -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
 
-<!-- Javascript -->
-<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-<script type="text/javascript" src="js/jquery.noconflict.js"></script>
-<script type="text/javascript" src="js/modernizr.2.7.1.min.js"></script>
-<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="js/jquery.placeholder.js"></script>
-<script type="text/javascript" src="js/jquery-ui.1.10.4.min.js"></script>
+    <!-- parallax -->
+    <script type="text/javascript" src="js/jquery.stellar.min.js"></script>
 
-<!-- Twitter Bootstrap -->
-<script type="text/javascript" src="js/bootstrap.js"></script>
+    <!-- waypoint -->
+    <script type="text/javascript" src="js/waypoints.min.js"></script>
 
-<!-- parallax -->
-<script type="text/javascript" src="js/jquery.stellar.min.js"></script>
+    <!-- Isotope -->
+    <script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
 
-<!-- waypoint -->
-<script type="text/javascript" src="js/waypoints.min.js"></script>
-
-<!-- Isotope -->
-<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
-
-<!-- load page Javascript -->
-<script type="text/javascript" src="js/theme-scripts.js"></script>
-<!--    <script type="text/javascript" src="js/scripts.js"></script>-->
+    <!-- load page Javascript -->
+    <script type="text/javascript" src="js/theme-scripts.js"></script>
+    <!--    <script type="text/javascript" src="js/scripts.js"></script>-->
 
 </body>
 
 </html>
-
